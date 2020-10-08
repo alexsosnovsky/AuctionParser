7e16aed499183a3ca79aef4475f0e4db55f37938
alexsosnovsky <60436249+alexsosnovsky@users.noreply.github.com>
2020-10-08 10:47:01 -0400

    Updates

---
+++ precache-manifest.bcdf3923d706195c24b81673b06060fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed33da668adcb795f83c0c85019ac647",
+    "url": "/auctionparser/index.html"
+  },
+  {
+    "revision": "7c8034ab21dbe726b8d5",
+    "url": "/auctionparser/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9601ebdca349ffaed021",
+    "url": "/auctionparser/static/js/2.eb9f6d0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/auctionparser/static/js/2.eb9f6d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8034ab21dbe726b8d5",
+    "url": "/auctionparser/static/js/main.97666a13.chunk.js"
+  },
+  {
+    "revision": "a020d186580288428cbc",
+    "url": "/auctionparser/static/js/runtime-main.bf935353.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auctionparser/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/auctionparser/precache-manifest.bcdf3923d706195c24b81673b06060fc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/auctionparser/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
